b176dbeab7294bbca6a08870c15fd32eb2d469f5
branislav999 <bbogosavac1999@gmail.com>
2023-12-05 22:56:22 -0700

    week09

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,35 @@
 using System;
+using System.Drawing;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Rectangle rec1 = new Rectangle("Green", 3, 4);
+        Circle cir1 = new Circle("Red", 7);
+        Circle cir2 = new Circle("Yellow", 4);
+        Rectangle rec2 = new Rectangle("Black", 2, 6);
+        Circle cir3 = new Circle("Blue", 4);
+
+        List<Shape> shapes = new List<Shape>();
+
+        shapes.Add(rec1);
+        shapes.Add(rec2);
+        shapes.Add(cir1);
+        shapes.Add(cir2);
+        shapes.Add(cir3);
+
+        foreach (Shape shape in shapes)
+        {
+            string color = shape.GetString();
+            double area = shape.GetArea();
+            Console.WriteLine($"Color: {color} -- Area: {area}");
+
+        }
+        
+
+        
+
+
     }
 }
